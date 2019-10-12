9df889ce1ebc7c1e34b0a993069a303989a45e71
qinbilei <qinbilei@thunder.com.cn>
2019-10-12 14:41:00 +0800

    add analysis

--- demos/analysis.py
+++ demos/analysis.py
@@ -50,5 +50,5 @@ for i in all_data:
             'mark': 0,
             'notice_time': i['notice_time']
         })
-    # del_pre_data(i['id'])
+    del_pre_data(i['id'])
 multi_add(AnalysisedStocks, res_data)
