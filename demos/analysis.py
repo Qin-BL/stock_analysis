a108066ca0a0188511ba74aefeefa87697e322d2
qinbilei <2528756899@qq.com>
2019-10-14 22:43:24 +0800

    1

--- demos/analysis.py
+++ demos/analysis.py
@@ -70,4 +70,4 @@ for i in all_data:
     del_pre_data(i['id'])
 logging.warning('finish,all is %d'%len(res_data))
 multi_add(AnalysisedStocks, res_data)
-mail('\n'.join(['股票代码:%s，股票名称:%s；'%(i['code'], i['name']) for i in res_data]))
+mail('\n'.join(['股票代码:%s，股票名称:%s；'%(i['code'], i['name']) for i in list(set(res_data))]))
