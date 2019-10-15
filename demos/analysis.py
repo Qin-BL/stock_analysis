7ba4304543f2fd2334704f44516fd26e72341e2c
qinbilei <qinbilei@thunder.com.cn>
2019-10-15 11:08:22 +0800

    fix bug

--- demos/analysis.py
+++ demos/analysis.py
@@ -68,6 +68,6 @@ for i in all_data:
         del_pre_data(i['id'])
     time.sleep(random.choice(range(2, 6)))
     del_pre_data(i['id'])
-logging.warning('finish,all is %d'%len(res_data))
+logging.warning('finish,all is %d' % len(res_data))
 multi_add(AnalysisedStocks, res_data)
-mail('\n'.join(['股票代码:%s，股票名称:%s；'%(i['code'], i['name']) for i in list(set(res_data))]))
+mail('\n'.join(['股票代码:%s，股票名称:%s；' % (i['code'], i['name']) for i in list(set(res_data))]))
