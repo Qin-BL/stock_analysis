0ca8378c17230812c4587c9d75052854c9474656
qinbilei <qinbilei@thunder.com.cn>
2020-06-09 20:24:37 +0800

    fix bug

--- demos/get_pre_data.py
+++ demos/get_pre_data.py
@@ -211,7 +211,6 @@ for version in versions:
             break
         for tr in tr_list:
             tmp = [i.strip() for i in tr.xpath('.//text()') if i.strip()]
-
             try:
                 tmp_day = datetime.datetime.strptime(tmp[3], '%Y-%m-%d')
             except:
